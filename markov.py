0c4eb95021d89e84402e7c0a1cd7bb4bb5163379
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-11 16:54:02 -0700

    Reads in 2 files, only begins at a capitalized word

--- markov.py
+++ markov.py
@@ -1,16 +1,20 @@
-"""Generate Markov text from text files."""
+"""Generate Markov text from 2 text files."""
 
 from random import choice
+import sys
 
 
-def open_and_read_file(file_path):
+def open_and_read_file(file_path1, file_path2):
     """Take file path as string; return text as string.
 
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
 
-    contents = open(file_path).read()
+    contents1 = open(file_path1).read()
+    contents2 = open(file_path2).read()
+
+    contents = contents1 + contents2
 
     return contents
 
@@ -35,7 +39,7 @@ def make_chains(text_string):
 
         >>> chains[('hi', 'there')]
         ['mary', 'juanita']
-        
+
         >>> chains[('there','juanita')]
         [None]
     """
@@ -46,15 +50,16 @@ def make_chains(text_string):
     #split() defaults to splitting at white space
 
     for i in range(len(words)):
-    # using range turns it into a list of indeces to loop through
+    # using range turns it into a list of indices to loop through
     # rather than looping over the words, allowing you to access
     # + 1 or +2 for example
+
         key = (words[i], words[(i + 1) % len(words)])
         # assigning a tuple to the var 'key', which is the first
-        # two words in the list of words. using % allows you to 
+        # two words in the list of words. using % allows you to
         # loop around from the end back to the beginning. this won't
         # have an effect until you are at the end of the list i.e.
-        # 5 % len(words) =  
+        # 5 % len(words) =
         if key not in chains:
         #if the key isn't in the dictionary:
             chains[key] = []
@@ -71,8 +76,21 @@ def make_text(chains):
     """Return text from chains."""
 
     random_keys = chains.keys()
-    random_key = choice(random_keys)
-    words = [random_key[0],random_key[1]]
+    #makes a list of all keys in the dictionary 'chains'
+
+    while True:
+
+        random_key = choice(random_keys)
+        #chooses a random key from the list of keys
+
+        words = [random_key[0], random_key[1]]
+        #stores the tuple in a list
+        if words[0].istitle() is not True:
+        #if the first word isn't title case, keep trying
+            continue
+        else:
+            break
+
 
     while True:
         third_word = choice(chains[random_key])
@@ -85,10 +103,12 @@ def make_text(chains):
     return " ".join(words[:-1])
 
 
-input_path = "green-eggs.txt"
+input_path_1 = sys.argv[1]
+input_path_2 = sys.argv[2]
 
-# Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+
+# Open the files and turn them into one long string
+input_text = open_and_read_file(input_path_1, input_path_2)
 
 # Get a Markov chain
 chains = make_chains(input_text)
